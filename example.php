6c99caf90bbd5d37c2541596f9cecc2a50babc23
as7bzx92dd <g04032016@mail.ru>
2016-10-27 17:04:11 +0400

    example

---
+++ example.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 27.10.2016
+ * Time: 16:00
+ */
+
+interface A
+{
+    public function summa($one, $two);// сумма двух чисел
+    public function umn($one, $two);// умножение двух чисел
+    public function minus($one, $two);// вычитание двух чисел
+    public function delenie($one, $two);// деление двух чисел
+
+    public function showres($one, $two);// вывод результата
+}
